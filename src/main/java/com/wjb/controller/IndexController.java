17d2fd0bb14d1704289510625a934ebcecb3f0e8
wjb <1523156110@qq.com>
2017-11-16 17:54:10 +0800

    底部和hacker.html

---
+++ src/main/java/com/wjb/controller/IndexController.java
@@ -0,0 +1,23 @@
+package com.wjb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by Administrator on 2017/11/16.
+ */
+@Controller
+public class IndexController {
+
+    @GetMapping("index")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("hacker")
+    public String hacker(){
+        return "pages/hacker";
+    }
+
+
+}
